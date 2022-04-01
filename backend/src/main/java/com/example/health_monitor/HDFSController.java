044fe4f0d5fe6b94a7a865af16c1bb8849fadea6
BerryYoghurt <jehadaly@gmail.com>
2022-04-01 14:41:45 +0200

    added backend

---
+++ backend/src/main/java/com/example/health_monitor/HDFSController.java
@@ -0,0 +1,17 @@
+package com.example.health_monitor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class HDFSController {
+
+    @GetMapping("/")
+    public HealthAnalytics getWindow(@RequestParam(value = "from") String startDate,
+                                     @RequestParam(value = "to") String endDate){
+        return // MapReduce.bla bla..????
+    }
+
+}
---
+++ backend/src/main/java/com/example/health_monitor/HealthAnalytics.java
@@ -0,0 +1,5 @@
+package com.example.health_monitor;
+
+public class HealthAnalytics {
+    // Make all attributes private and final and include getters, see https://spring.io/guides/gs/rest-service/
+}
