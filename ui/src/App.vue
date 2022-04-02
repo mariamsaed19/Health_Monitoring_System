db8dbe7378f8f61e97cc9e88f8ada1d0c6e58257
mariamsaed19 <mariamzaho4@gmail.com>
2022-04-02 19:23:57 +0200

    ui

---
+++ ui/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-container>
+    <Datepicker/>
+    <v-btn
+      rounded
+      color="primary"
+      dark
+      sm="6"
+    >
+      Show statistics
+    </v-btn>
+    <Table/>
+    </v-container>
+  </v-app> 
+</template>
+
+<script>
+import Datepicker from './components/Datepicker.vue';
+//import Card from './components/Card.vue';
+import Table from './components/Table.vue'
+export default {
+  name: 'App',
+
+  components :{
+    Datepicker ,
+    Table
+
+
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ ui/src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-date-picker
+            v-model="date"
+            @input="menu2 = false"
+    ></v-date-picker>
+</template>
+
+<script>
+  export default {
+    name: 'Card',
+
+    data: () => ({
+     
+    }),
+  }
+</script>
